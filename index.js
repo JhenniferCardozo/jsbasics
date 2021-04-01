47b754e635c77f2eb192746ecb34a1c741b8a161
Jhennifer Cardozo <73983848+JhenniferCardozo@users.noreply.github.com>
2021-04-01 15:09:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -207,3 +207,18 @@ numberOfBottles--;
     }
 }
 
+// 12- Function to calculate how many days, weeks and months we have left if we live until 90 years old
+
+function lifeInWeeks(age) {
+    
+var yearsRemaining = 90 - age;
+var days = yearsRemaining * 365;
+var weeks = yearsRemaining * 52;
+var months = yearsRemaining * 12;
+
+console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+    
+}
+
+lifeInWeeks(38);
+
